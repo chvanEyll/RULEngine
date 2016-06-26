b97b80460e259c212a987f6106cbb79fae08aaa7
Félix Pelletier <felixpelletier23@gmail.com>
2016-06-25 20:03:44 -0400

    Fixed tests for new architecture

--- tests/util/test_geometry.py
+++ tests/util/test_geometry.py
@@ -1,17 +1,17 @@
 #Under MIT License, see LICENSE.txt
 import unittest
 import math as m
-import RULEngine.Util.Vector
+import RULEngine.util.geometry
 
 
 class TestVector(unittest.TestCase):
 
     def setUp(self):
-        self.vector = RULEngine.Util.Vector.Vector()
-        self.vector2 = RULEngine.Util.Vector.Vector(30, m.pi)
-        self.vector3 = RULEngine.Util.Vector.Vector(30, m.pi)
-        self.vector4 = RULEngine.Util.Vector.Vector(2016, m.pi/4)
-        self.vector5 = RULEngine.Util.Vector.Vector(40, m.pi/2)
+        self.vector = RULEngine.util.geometry.Vector()
+        self.vector2 = RULEngine.util.geometry.Vector(30, m.pi)
+        self.vector3 = RULEngine.util.geometry.Vector(30, m.pi)
+        self.vector4 = RULEngine.util.geometry.Vector(2016, m.pi/4)
+        self.vector5 = RULEngine.util.geometry.Vector(40, m.pi/2)
 
     def test_getlength(self):
         self.assertEqual(self.vector.length, 1.0)
@@ -66,86 +66,86 @@ class TestVector(unittest.TestCase):
         self.assertEqual(b00l, False)
 
     def test_addposition(self):
-        p = RULEngine.Util.Position.Position()
+        p = RULEngine.util.geometry.Position()
         p2 = self.vector5 + p
-        reference = RULEngine.Util.Position.Position(0, 40)
+        reference = RULEngine.util.geometry.Position(0, 40)
         self.assertAlmostEqual(p2.x, reference.x, 10)
         self.assertAlmostEqual(p2.y, reference.y, 10)
 
     def test_addpose(self):
-        p = RULEngine.Util.Pose.Pose()
+        p = RULEngine.util.geometry.Pose()
         p2 = self.vector4 + p
-        reference = RULEngine.Util.Pose.Pose(RULEngine.Util.Position.Position(2016*m.cos(m.pi/4), 2016*m.sin(m.pi/4)), m.pi/4)
+        reference = RULEngine.util.geometry.Pose(RULEngine.util.geometry.Position(2016*m.cos(m.pi/4), 2016*m.sin(m.pi/4)), m.pi/4)
         self.assertAlmostEqual(p2.position.x, reference.position.x, 10)
         self.assertAlmostEqual(p2.position.y, reference.position.y, 10)
         self.assertAlmostEqual(p2.orientation, reference.orientation,10)
 
     def test_addvector(self):
         a = self.vector2 + self.vector5
-        b = RULEngine.Util.Vector.Vector(m.sqrt(30**2 + 40**2), m.atan2(40, -30))
+        b = RULEngine.util.geometry.Vector(m.sqrt(30**2 + 40**2), m.atan2(40, -30))
         self.assertEqual(a, b)
 
     def test_raddposition(self):
-        p = RULEngine.Util.Position.Position()
+        p = RULEngine.util.geometry.Position()
         p2 = p + self.vector5
-        reference = RULEngine.Util.Position.Position(0, 40)
+        reference = RULEngine.util.geometry.Position(0, 40)
         self.assertAlmostEqual(p2.x, reference.x, 10)
         self.assertAlmostEqual(p2.y, reference.y, 10)
 
     def test_raddpose(self):
-        p = RULEngine.Util.Pose.Pose()
+        p = RULEngine.util.geometry.Pose()
         p2 = p + self.vector4
-        reference = RULEngine.Util.Pose.Pose(RULEngine.Util.Position.Position(2016*m.cos(m.pi/4), 2016*m.sin(m.pi/4)), m.pi/4)
+        reference = RULEngine.util.geometry.Pose(RULEngine.util.geometry.Position(2016*m.cos(m.pi/4), 2016*m.sin(m.pi/4)), m.pi/4)
         self.assertAlmostEqual(p2.position.x, reference.position.x, 10)
         self.assertAlmostEqual(p2.position.y, reference.position.y, 10)
         self.assertAlmostEqual(p2.orientation, reference.orientation,10)
 
     def test_iadd(self):
         self.vector += self.vector4
-        self.assertEqual(self.vector, RULEngine.Util.Vector.Vector(2016.7072307456, 0.78504753898))
+        self.assertEqual(self.vector, RULEngine.util.geometry.Vector(2016.7072307456, 0.78504753898))
 
     def test_sub(self):
         a = self.vector5 - self.vector3
-        self.assertEqual(a, RULEngine.Util.Vector.Vector(50, 0.9272952180))
+        self.assertEqual(a, RULEngine.util.geometry.Vector(50, 0.9272952180))
 
     def test_sub2(self):
         a = self.vector5 - self.vector4
-        self.assertEqual(a, RULEngine.Util.Vector.Vector(1987.9169545859, -2.3704230653))
+        self.assertEqual(a, RULEngine.util.geometry.Vector(1987.9169545859, -2.3704230653))
 
     def test_subzero(self): #fatality, Subzero wins
         a = self.vector - self.vector
-        self.assertEqual(a, RULEngine.Util.Vector.Vector(0, 0))
+        self.assertEqual(a, RULEngine.util.geometry.Vector(0, 0))
 
     def test_isub(self):
         self.vector5 -= self.vector4
-        self.assertEqual(self.vector5, RULEngine.Util.Vector.Vector(1987.9169545859, -2.3704230653))
+        self.assertEqual(self.vector5, RULEngine.util.geometry.Vector(1987.9169545859, -2.3704230653))
 
     def test_neg(self):
-        self.assertEqual(-self.vector4, RULEngine.Util.Vector.Vector(2016, -m.pi/4))
+        self.assertEqual(-self.vector4, RULEngine.util.geometry.Vector(2016, -m.pi/4))
 
     def test_mul(self):
-        self.assertEqual(self.vector5*2, RULEngine.Util.Vector.Vector(80, m.pi/2))
+        self.assertEqual(self.vector5*2, RULEngine.util.geometry.Vector(80, m.pi/2))
 
     def test_mul0(self):
-        self.assertEqual(self.vector5*0, RULEngine.Util.Vector.Vector(0, m.pi/2))
+        self.assertEqual(self.vector5*0, RULEngine.util.geometry.Vector(0, m.pi/2))
 
     def test_mulneg(self):
-        self.assertEqual(self.vector3*-1, RULEngine.Util.Vector.Vector(30, -m.pi))
+        self.assertEqual(self.vector3*-1, RULEngine.util.geometry.Vector(30, -m.pi))
 
     def test_rmul(self):
-        self.assertEqual(2*self.vector5, RULEngine.Util.Vector.Vector(80, m.pi/2))
+        self.assertEqual(2*self.vector5, RULEngine.util.geometry.Vector(80, m.pi/2))
 
     def test_imul(self):
         self.vector4 *= 8
-        self.assertEqual(self.vector4, RULEngine.Util.Vector.Vector(16128, m.pi/4))
+        self.assertEqual(self.vector4, RULEngine.util.geometry.Vector(16128, m.pi/4))
 
     def test_imul0(self):
         self.vector4 *= 0
-        self.assertEqual(self.vector4, RULEngine.Util.Vector.Vector(0, m.pi/4))
+        self.assertEqual(self.vector4, RULEngine.util.geometry.Vector(0, m.pi/4))
 
     def test_imulneg(self):
         self.vector4 *= -2
-        self.assertEqual(self.vector4, RULEngine.Util.Vector.Vector(4032, -m.pi/4))
+        self.assertEqual(self.vector4, RULEngine.util.geometry.Vector(4032, -m.pi/4))
 
     def test_str(self):
         string = str(self.vector)
